f76bddbfde67ac9a939c359553ef111846489eac
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-15 13:03:44 -0600

    added router.get function to todos.router

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+router.get("/", (req, res)=>{
+    let queryText = `SELECT * FROM "todos";`
 
+    pool.query(queryText)
+        .then((result)=>{
+            console.log('in GET /todos resukt.rows', result.rows)
+            res.send(result.rows)
+        })
+        .catch((error)=>{
+            console.log('error in /GET router', error)
+            res.sendStatus(500)
+        })
+})
 
 
 module.exports = router;

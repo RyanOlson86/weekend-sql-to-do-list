bc09c3b4a2b57e386387f972930b70a5664ce021
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-15 13:19:20 -0600

    finished router functions and tested with postman

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -15,5 +15,59 @@ router.get("/", (req, res)=>{
         })
 })
 
+router.post("/", (req, res)=>{
+    let queryText = ` 
+    INSERT INTO "todos" ("text") VALUES ($1);
+    `
+    let queryParams = [req.body.text]
+    console.log('req.body', req.body)
+
+    pool.query(queryText, queryParams)
+        .then((result)=>{
+            console.log('in POST /todos')
+            res.sendStatus(201)
+        })
+        .catch((error)=>{
+            console.log('error in /POST router', error)
+            res.sendStatus(500)
+        })
+})
+
+router.put("/:id", (req, res)=>{
+    let queryText = ` 
+    UPDATE "todos" SET "isComplete" = true WHERE "id"=$1;
+    `
+    let queryParams = [req.params.id]
+    console.log('req.params.id', req.params.id)
+
+    pool.query(queryText, queryParams)
+        .then((result)=>{
+            console.log('in PUT /todos')
+            res.sendStatus(201)
+        })
+        .catch((error)=>{
+            console.log('error in /PUT router', error)
+            res.sendStatus(500)
+        })
+})
+
+router.delete("/:id", (req, res)=>{
+    let queryText = ` 
+    DELETE FROM "todos" WHERE "id" = $1;
+    `
+    let queryParams = [req.params.id]
+    console.log('req.params.id', req.params.id)
+
+    pool.query(queryText, queryParams)
+        .then((result)=>{
+            console.log('in PUT /todos')
+            res.sendStatus(200)
+        })
+        .catch((error)=>{
+            console.log('error in /PUT router', error)
+            res.sendStatus(500)
+        })
+})
+
 
 module.exports = router;
